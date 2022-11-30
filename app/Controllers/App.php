4891fa27bd49fe124a0eb4b40d73f408a43435c8
John McCole <jmccole83@users.noreply.github.com>
2022-11-30 23:43:52 +0000

    + Added support for Bootstrap nav walker. + Added SCSS file nav walker dropdown menus.

--- app/Controllers/App.php
+++ app/Controllers/App.php
@@ -6,6 +6,19 @@ use Sober\Controller\Controller;
 
 class App extends Controller
 {
+    /**
+     * Primary Nav Menu arguments
+     * @return array
+     */
+    public function primarymenu() {
+      $args = array(
+        'theme_location'        => 'primary_navigation',
+        'menu_class'              => 'navbar-nav',
+        'walker'                        => new \App\wp_bootstrap4_navwalker(),
+      );
+      return $args;
+    }
+
     public function logo()
     {
       return get_field('logo', 'option');
@@ -65,45 +78,4 @@ class App extends Controller
     public function hamburger_breakpoint() {
       return get_field('hamburger_breakpoint', 'option');
     }
-}
-// Rename posts in the admin menu
-if( function_exists('acf_add_options_page') ) {
-
-  $rename = get_field('rename_posts', 'option');
-
-  if($rename) {
-
-    add_action( 'admin_menu', function() {
-      $new_name = get_field('posts_new_name', 'option');
-      $singular_name = get_field('post_singular_name', 'option');
-      global $menu;
-      global $submenu;
-      $submenu['edit.php'][5][0]   = $new_name;
-      $submenu['edit.php'][10][0]  = 'Add new '.$singular_name;
-      $submenu['edit.php'][16][0]  = 'Tags';
-      $menu[5][0]                  = $new_name;
-    }  );
-
-
-    // Rename the buttons/labels in the Post section
-    add_action( 'init', function() {
-      $new_name = get_field('posts_new_name', 'option');
-      $singular_name = get_field('post_singular_name', 'option');
-      global $wp_post_types;
-      $labels                      = &$wp_post_types['post']->labels;
-      $labels->name                = $new_name;
-      $labels->singular_name       = $singular_name;
-      $labels->add_new             = 'Add '.$singular_name;
-      $labels->add_new_item        = 'Add '.$singular_name;
-      $labels->edit_item           = 'Edit '.$singular_name;
-      $labels->new_item            = $singular_name;
-      $labels->view_item           = 'View '.$new_name;
-      $labels->search_items        = 'Search '.$new_name;
-      $labels->not_found           = 'No '.$new_name.' found';
-      $labels->not_found_in_trash  = 'No '.$new_name.' found in Trash';
-      $labels->all_items           = 'All '.$new_name;
-      $labels->menu_name           = $new_name;
-      $labels->name_admin_bar      = $new_name;
-    }  );
-  }
 }
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -25,8 +25,13 @@
   <div class="container d-flex align-items-center justify-content-between">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary d-flex align-items-center justify-content-end">
+      {{--
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      @endif
+      --}}
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu($primarymenu) !!}
       @endif
       <button class="navbar-toggler hamburger {!! $hamburger_style !!} {!! $hamburger_breakpoint !!}" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
         <span class="hamburger-box">
