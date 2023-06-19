b71d69dfb57302612123cb21d288afe905b7b08d
AzureAD\JohnMcCole <john@productionbunker.com>
2023-06-19 09:25:01 +0100

    + Added fonts and fonts stylesheet. + Added fonts to _typography.scss. + Enqueued fonts.css in setup.php.

--- app/setup.php
+++ app/setup.php
@@ -11,6 +11,8 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('sage/fonts.css', get_theme_file_uri().'/resources/fonts/fonts.css', false, null);
+    
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
